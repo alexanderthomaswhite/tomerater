44c5981a0176ae68e932e5204e7b91fbbe9adc01
awhite <awhite@marchex.com>
2018-11-26 17:25:26 -0800

    add book class

--- TomeRater.py
+++ TomeRater.py
@@ -12,13 +12,42 @@ class User(object):
 #change users email
   def change_email(self, address):
     self.email = address
-    print(self.name + "'s email has been changed to " + address) 
+    print(self.name + "'s email has been changed to " + address + ".") 
 
   def __repr__(self):
-    "user: self.name, email: self.email, books read: len(self.book)"  
+    return user: self.name, email: self.email, books read: len(self.book)  
 
   def __eq__(self, other_user):
     if self.name == other_user.self.name and self.email == other_user.self.email:
       return True
     else:
-      return False`  
+      return False
+
+#Book class, will store info about the books themselves
+class Book(object):
+  def __init__(self, title, isbn):
+    self.title = title
+    self.isbn = isbn
+    self.ratings = {}
+
+  def get_title(self):
+    return self.title
+
+  def get_isbn(self):
+    return self.isbn
+
+  def set_isbn(self, isbn):
+   self.isbn = isbn
+   print(self.title + "'s isbn number has been reset to " + self.isbn + ".")
+ 
+  def add_rating(self, rating):
+    if rating >= 0 and rating <= 4:
+      self.ratings.append(rating)
+    else:
+      print("Invalid rating! (must be between 0 and 4)")
+
+  def __eq__(self, other_book):
+    if self.title == other_book.self.title and self.isbn == other_book.self.isbn:
+      return True
+    else:
+      return False

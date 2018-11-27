a595cb5f399e1003a00802750bb14f8168bcb167
awhite <awhite@marchex.com>
2018-11-26 16:49:15 -0800

    build user class methods

--- TomeRater.py
+++ TomeRater.py
@@ -1,15 +1,24 @@
 class User(object):
-    def __init__(self, name, email):
-        pass
+#user constructor, will store name, email, and dictionary of books
+  def __init__(self, name, email):
+    self.name = name
+    self.email = email
+    self.book = {}  
 
-    def get_email(self):
-        pass
+#return users email
+  def get_email(self):
+    return self.email  
 
-    def change_email(self, address):
-        pass
+#change users email
+  def change_email(self, address):
+    self.email = address
+    print(self.name + "'s email has been changed to " + address) 
 
-    def __repr__(self):
-        pass
+  def __repr__(self):
+    "user: self.name, email: self.email, books read: len(self.book)"  
 
-    def __eq__(self, other_user):
-        pass
+  def __eq__(self, other_user):
+    if self.name == other_user.self.name and self.email == other_user.self.email:
+      return True
+    else:
+      return False`  

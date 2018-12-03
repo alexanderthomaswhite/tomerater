b8fb9738637b3d929749febbf65c054edde6d18d
awhite <awhite@marchex.com>
2018-12-03 15:31:39 -0800

    add tomerater class

--- TomeRater.py
+++ TomeRater.py
@@ -3,20 +3,20 @@ class User(object):
   def __init__(self, name, email):
     self.name = name
     self.email = email
-    self.book = {}  
+    self.books = {}  
 
-#Return email value
+#Returns email value
   def get_email(self):
     return self.email  
 
-#Change email value
+#Changes email value
   def change_email(self, address):
     self.email = address
     print(self.name + "'s email has been changed to " + address + ".") 
 
 #String representation
   def __repr__(self):
-    return "user: {name}, email: {email}, books read: {num_books)}".format(name = self.name, email = self.email, num_books = len(self.book)) 
+    return "user: {name}, email: {email}, books read: {num_books)}".format(name = self.name, email = self.email, num_books = len(self.books)) 
 
 #Comparison method
   def __eq__(self, other_user):
@@ -25,6 +25,19 @@ class User(object):
     else:
       return False
 
+#Adds key value pairs to self.books
+  def read_book(self, book, rating = None):
+    self.books[book] = rating
+
+#Returns average of ratings in self.books
+  def get_average_rating(self):
+    total = 0
+    num_books = 0
+    for value in values.self.books():
+      total += value
+      num_books += 1
+    return total / num_books
+    
 
 #Book class,
 class Book(object):
@@ -60,6 +73,17 @@ class Book(object):
     else:
       return False
 
+#Returns average of self.ratings
+  def get_average_rating(self):
+    total = 0
+    for rating in self.ratings:
+      total += rating
+    return total / len(self.ratings)
+
+#Makes Book object hashable
+  def __hash__(self):
+    return hash((self.title, self.isbn))
+
 
 #Fiction, subclass of Book
 class Fiction(Book):
@@ -67,11 +91,11 @@ class Fiction(Book):
     Book.init(self, title, isbn)
     self.author = author
 
-#Return author
+#Returns author
   def get_author(self):
     return self.author
 
-#Set pleasing string representation
+#String representation
   def __repr__(self):
     return "{title} by {author}".format(title = self.title, author = self.author)
 
@@ -83,11 +107,11 @@ class Non_Fiction(book):
     self.subject = subject
     self.level = level
 
-#Return subject
+#Returns subject
   def get_subject(self)
     return self.subject
 
-#Return level
+#Returns level
   def get_level(self):
     return self.level
 
@@ -96,5 +120,52 @@ class Non_Fiction(book):
     return "{title}, a {level} manual on {subject}".format(title = self.title, level = self.level, subject = self.subject)
 
 
+
+#TomeRater class
+class TomeRater(object)
+  def __init__(self):
+    self.users = {}
+    self.books = {}
+
+#Creates book object
+  def create_book(self, title, isbn):
+    return Book(title, isbn)
+
+  def create_novel(self, title, author, isbn):
+    return Fiction(title, author, isbn)
+
+  def create_non_fiction(self, title, subject, level, isbn):
+    return Non_Fiction(title, subject, level, isbn)
+
+  def add_book_to_user(self, book, email, rating = None):
+    user = self.users.get(email, "No user with email: {email}".format(email = email)
+    if user:
+      user.read_book(book, rating)
+      book.add_rating(rating)
+      if book in self.books:
+        self.books[book] += 1
+      else:
+        self.books[book] = 1
+  
+  def add_user(name, email, user_books = None):
+    if email not in self.users:
+      self.users[email] = User(name, email)
+      if user_books is not None:
+        for book in user_books:
+          self.add_book_to_user(book, email)
+   else:
+     print("{name} is already a user.".format(name = name)
+
+
+  def print_catalog(self):
+    for book in self.books.keys():
+      print(book)
+   
+  def print_users(self):
+    for users in self.users.values():
+      print(user)
+
+  def most_read_book(self):
+    
 
 
